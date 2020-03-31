78384b2e6e03dff31e027ae04510bf52d9f19ed1
Julien Bisconti <hello@moritzgut.de>
2020-03-31 11:43:00 +0200

    Seperate PhpMyAdmin container approach

--- bin/services/pma.js
+++ bin/services/pma.js
@@ -1,6 +1,65 @@
 #! /usr/bin/env node
-const start = (inquiry) => {
-  console.log('Start phpmyadmin');
+// const { exec } = require('child_process');
+const chalk = require('chalk');
+
+const Docker = require('dockerode');
+const docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+const { tools } = require('../utils');
+const { service } = require('./services');
+
+const start = async (inquiry) => {
+  const mysql = await service.db().then(info => info);
+
+  if (mysql.status) {
+    const databaseName = mysql.info.Names[0].substr(1);
+    const network = mysql.info.NetworkSettings.Networks[mysql.website + '_default'];
+    const pmaContainer = {
+      Image: 'phpmyadmin/phpmyadmin',
+      Tty: false,
+      name: 'phpmyadmin',
+      HostConfig: {
+        AutoRemove: true,
+        Links: [ 
+          'db:db' 
+        ],
+        NetworkMode: mysql.website + '_default',
+        PortBindings: {
+          '80/tcp': [
+            { 
+              HostIp: '0.0.0.0',
+              HostPort: '4000' 
+            }
+          ]
+        },
+      }
+    }
+
+    tools.handle.log(
+      'Trying to connect database interface to '
+        + '\n'
+        + 'Container: ' + chalk.yellow(databaseName) 
+        + '\n' 
+        + 'Network: ' + chalk.yellow(network.NetworkID), 
+      'normal'
+    );
+
+    docker.createContainer(
+      {...pmaContainer}, 
+      (err, container) => {
+        if (err) { tools.handle.error('\n' + err); return; }
+        
+        container.start((err, container) => {
+          if (err) { tools.handle.error(err); }
+          tools.handle.log(
+            '\n'
+              + 'PhpMyAdmin is running!', 
+            'success'
+          );
+        });
+      }
+    );
+  }
 }
 
 const stop = (inquiry) => {
--- bin/services/services.js
+++ bin/services/services.js
@@ -2,11 +2,15 @@
 const { tools } = require('../utils');
 
 const wordpress = async () => {
+  const regex = /\/(.*)(\_wordpress_1)/;
   const service = await tools.check.activity('wordpress')
     .then(res => res)
     .catch(err => tools.handle.error(err))
 
-  return service;
+  return {
+    website: regex.exec(service.info.Names[0])[1],
+    ...service
+  };
 }
 
 const phpmyadmin = async () => {
@@ -17,11 +21,28 @@ const phpmyadmin = async () => {
   return service;
 }
 
+const mysql = async () => {
+  const regex = /\/(.*)(\_database_1)/;
+  const service = await tools.check.activity('mysql')
+    .then(res => res)
+    .catch(err => tools.handle.error(err))
+
+  
+
+  return {
+    website: regex.exec(service.info.Names[0])[1],
+    ...service
+  };
+}
+
 exports.service = {
   wordpress: wordpress,
   wp: wordpress,
   phpmyadmin: phpmyadmin,
-  pma: phpmyadmin
+  pma: phpmyadmin,
+  mysql: mysql,
+  db: mysql
 }
 exports.wordpress = wordpress;
 exports.phpmyadmin = phpmyadmin;
+exports.mysql = mysql;
